9d7e74be9330fb77a190595b714525b4d6916baf
Jan Kalina <jan.kalina@fantom.foundation>
2022-06-10 17:24:40 +0200

    Metrics - peers sync status

--- gossip/handler.go
+++ gossip/handler.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/Fantom-foundation/lachesis-base/gossip/dagprocessor"
@@ -67,6 +68,10 @@ var (
 	peerWorkedCounter = metrics.GetOrRegisterCounter("lachesis/peer/worked", nil)
 	peerIdleTimer = metrics.GetOrRegisterTimer("lachesis/peer/idletimer", nil)
 	peerWorkedTimer = metrics.GetOrRegisterTimer("lachesis/peer/workedtimer", nil)
+	peersSyncedGauge = metrics.GetOrRegisterGauge("p2p/peers/synced", nil)
+	peersMoreSyncedGauge = metrics.GetOrRegisterGauge("p2p/peers/moresynced", nil)
+	peersSendingGauge = metrics.GetOrRegisterGauge("p2p/peers/sending", nil)
+	lastMetricsUpdate = int64(0)
 )
 
 func errResp(code errCode, format string, v ...interface{}) error {
@@ -769,6 +774,17 @@ func (h *handler) highestPeerProgress() PeerProgress {
 	return max
 }
 
+func (h *handler) updateMetrics() {
+	if atomic.LoadInt64(&lastMetricsUpdate) < time.Now().Add(-5 * time.Second).Unix() {
+		atomic.StoreInt64(&lastMetricsUpdate, time.Now().Unix())
+		myBlock := h.store.GetBlockState().LastBlock.Idx
+		maxBlock := h.highestPeerProgress().LastBlockIdx
+		peersSyncedGauge.Update(h.peers.PeersSyncedAtLeast(maxBlock - 100))
+		peersMoreSyncedGauge.Update(h.peers.PeersSyncedAtLeast(myBlock))
+		peersSendingGauge.Update(h.peers.PeersSendingEvents(20 * time.Minute))
+	}
+}
+
 // handle is the callback invoked to manage the life cycle of a peer. When
 // this function terminates, the peer is disconnected.
 func (h *handler) handle(p *peer) error {
@@ -951,6 +967,7 @@ func (h *handler) handleEvents(p *peer, events dag.Events, ordered bool) {
 	notifyAnnounces := func(ids hash.Events) {
 		_ = h.dagFetcher.NotifyAnnounces(peer.id, eventIDsToInterfaces(ids), now, requestEvents)
 	}
+	p.MarkReceivedEvent()
 	_ = h.dagProcessor.Enqueue(peer.id, notTooHigh, ordered, notifyAnnounces, nil)
 }
 
@@ -1004,6 +1021,7 @@ func (h *handler) handleMsg(p *peer) error {
 		if progress.Epoch == myEpoch {
 			h.syncStatus.MarkMaybeSynced()
 		}
+		h.updateMetrics()
 
 	case msg.Code == EvmTxsMsg:
 		// Transactions arrived, make sure we have a valid and fresh graph to handle them
